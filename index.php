4d0d1f2769bb006e26dfa03808b2ef5e85a28c64
Derek Moeller <derek@goremedy.com>
2014-04-24 15:49:02 -0700

    Lets Give This a Shot.

---
+++ index.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en">
+<![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" lang="en">
+<![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en">
+<![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="en" class="no-js">
+  <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Derek Moeller</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
+    <script src="/js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="//use.typekit.net/ldf0tzf.js"></script>
+    <script>
+      try{Typekit.load();}catch(e){}
+      
+    </script>
+  </head>
+  <body>
+    <div id="loading"><img src="img/load.gif"></div>
+    <div id="intro">
+      <!-- Header-->
+      <header class="container pull-left">
+        <div class="row">
+          <div class="span9">
+            <!-- Logo-->
+            <div class="logo">
+              <h1>Derek Moeller</h1>
+              <h2>Excellent Customer Service Meets Tech Space  <span class="bullet">&#149;</span> <span class="gold">Burlingame, CA</span></h2>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="span3">
+            <h4>Phone</h4>
+            <p>
+              Call me anytime: <br><a href="tel://602-769-8798">602-769-8798</a>
+              
+            </p>
+          </div>
+          <div class="span3">
+            <h4>Email</h4>
+            <p>
+              Questions &amp; Inquiries: <br><a href="mailto:helloworld@voteforderek.org">helloworld@voteforderek.org</a>
+              
+            </p>
+          </div>
+          <div class="span3">
+            <h4>Social</h4>
+            <p>
+              <a href="http://twitter.com/derekmolar">Follow me on Twitter</a><br><a href="http://github.com/moellerderek">Look at my projects</a>
+              </p>
+          </div>
+        </div>
+      
+                <div class="back"><a href="#intro">take me back to the top</a></div>
+              </div>
+              <div class="span2"><img src="img/logo.png"></div>
+              <div class="span3">
+                <div class="copy">&copy; 2014 all rights reserved</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>
+      window.jQuery || document.write('<script src="/js/vendor/jquery-1.10.2.min.js"><\\/script>')
+      
+    </script>
+    <script src="js/plugins.js?v=3"></script>
+    <script src="js/vendor/superscrollorama.js"></script>
+    <script src="js/main.min.js?v=3"></script>
+    <script>
+      var _gaq=[['_setAccount','UA-4117562-2'],['_trackPageview'],['_trackPageLoadTime']];
+      (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+      s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
+  </body>
+</html>
